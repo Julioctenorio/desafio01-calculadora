cca6d6dc345c6fcae1b964ba621ccccfd89af8e2
Julio Cesar <julio.cesar_3@hotmail.com>
2022-12-06 22:42:57 +0000

    Projeto-Calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const InpuContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+    margin: 0 10px
+    
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+    input{
+        width: 100%;
+        height: 75px;
+        padding: 0 10px;
+        background-color: #AAAAFF;
+        border: 0;
+        float: right;
+        clear: both;
+        color: #FFFFFF
+
+        font-size: 24px;
+        font-family: 'Roboto';
+    }
+`
